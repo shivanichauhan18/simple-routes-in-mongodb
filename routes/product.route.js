0448b520e917d4fe30cb050f9e0d7a82372ff425
shivanichauhan18 <shivanic18@navgurukul.org>
2020-07-30 19:51:34 +0530

    created simple routes in mongodb

---
+++ routes/product.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const product_controller = require('../controllers/product.controller');
+router.post('/create', product_controller.product_create);
+router.get('/:name', product_controller.product_details);
+
+
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', product_controller.test);
+module.exports = router;
